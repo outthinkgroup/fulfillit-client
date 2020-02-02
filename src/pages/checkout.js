7abed08a6de7ff2bd0acc362acd301495b11b7e4
Joshua Kennedy <josh@outthinkgroup.com>
2020-02-02 18:04:13 -0500

    fix some ssr checks

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -6,13 +6,15 @@ import CheckoutStyledWrapper from "../designSystem/styles/MyCardElement"
 import SingleFormLayout from "../Layouts/SingleFormLayout"
 const Checkout = () => (
   <SingleFormLayout>
-    <CheckoutStyledWrapper>
-      <StripeProvider apiKey="pk_test_gLXHhM133Hr3wT95bfbTbjY300a67NU0FP">
-        <Elements>
-          <CheckoutForm />
-        </Elements>
-      </StripeProvider>
-    </CheckoutStyledWrapper>
+    {typeof window !== "undefined" && (
+      <CheckoutStyledWrapper>
+        <StripeProvider apiKey="pk_test_gLXHhM133Hr3wT95bfbTbjY300a67NU0FP">
+          <Elements>
+            <CheckoutForm />
+          </Elements>
+        </StripeProvider>
+      </CheckoutStyledWrapper>
+    )}
   </SingleFormLayout>
 )
 
