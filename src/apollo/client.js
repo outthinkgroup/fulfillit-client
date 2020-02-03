c992b91b206f353c4197ae3f474bdc1a0b7c3e06
Joshua Kennedy <josh@outthinkgroup.com>
2020-02-02 20:37:17 -0500

    added env vars

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -5,7 +5,8 @@ export const isBrowser = () => typeof window !== "undefined"
 const token = isBrowser() && window.localStorage.getItem("token")
 console.log(token)
 export const client = new ApolloClient({
-  uri: `https://fulfillit.mightyoak.co/graphql`, //?this needs to be an env var
+  //uri: `https://fulfillit.mightyoak.co/graphql`, //?this needs to be an env var
+  uri: process.env.GATSBY_WP_URL,
   fetch,
   request: operation => {
     const token = localStorage.getItem("token")
