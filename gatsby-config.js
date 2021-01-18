973c0d4dd3ef49243f13d79ed0c998bea41d2cf1
Joshua Kennedy <josh@outthinkgroup.com>
2021-01-18 15:00:45 -0500

    upgraded packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,18 +10,17 @@ module.exports = {
 
     `gatsby-plugin-stripe`,
 
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Fulfillit By Outthink`,
-        short_name: `Fulfillit`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#000000`,
-        display: `minimal-ui`,
-        //icon: `src/images/.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `Fulfillit By Outthink`,
+    //     short_name: `Fulfillit`,
+    //     start_url: `/`,
+    //     background_color: `#ffffff`,
+    //     theme_color: `#000000`,
+    //     display: `minimal-ui`,
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     //`gatsby-plugin-offline`,
